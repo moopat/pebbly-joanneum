a4e12f1dfdefcc83ce9f423fcd3f54a86bc2d972
Markus Deutsch <moopat@users.noreply.github.com>
2014-11-29 23:23:19 +0100

    Added placeholders for lecture data.

--- src/main.c
+++ src/main.c
@@ -2,7 +2,9 @@
 	
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static TextLayer *s_schedule_layer;
 static GFont s_time_font;
+static GFont s_schedule_font;
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
 
@@ -46,15 +48,31 @@ static void main_window_load(Window *window) {
 	
 	s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_TREBUCHET_42));
 	text_layer_set_font(s_time_layer, s_time_font);
-  
 	text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-	
 	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+
+	s_schedule_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_TREBUCHET_18));
+	s_schedule_layer = text_layer_create(GRect(5, 107, window_bounds.size.w-10, window_bounds.size.h - 102));
+	text_layer_set_background_color(s_schedule_layer, GColorClear);
+	text_layer_set_text_color(s_schedule_layer, GColorBlack);
+	text_layer_set_font(s_schedule_layer, s_schedule_font);
+	text_layer_set_text_alignment(s_schedule_layer, GTextAlignmentLeft);
+	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_schedule_layer));
+	text_layer_set_text(s_schedule_layer, "ENG02\n8:30 - 10:00\nSR66");
+																		
+
 }
 
 static void main_window_unload(Window *window) {
 	text_layer_destroy(s_time_layer);
+	text_layer_destroy(s_schedule_layer);
+	fonts_unload_custom_font(s_schedule_font);
 	fonts_unload_custom_font(s_time_font);
+	// Destroy GBitmap
+	gbitmap_destroy(s_background_bitmap);
+
+	// Destroy BitmapLayer
+	bitmap_layer_destroy(s_background_layer);
 }
 
 static void init(){
